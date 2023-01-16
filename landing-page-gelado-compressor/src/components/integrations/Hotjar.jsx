0cba3a440106a23dfc7482207ef59681499c2ecb
Abraão Morais <moraisabraao@gmail.com>
2023-01-16 00:44:14 -0300

    hotjar

---
+++ landing-page-gelado-compressor/src/components/integrations/Hotjar.jsx
@@ -0,0 +1,11 @@
+import Script from 'next/script'
+import React from 'react'
+
+export default function Hotjar(){
+
+  return (
+    <Script id='teste'>
+      {`(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:3323558,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+    </Script>
+  )
+}
