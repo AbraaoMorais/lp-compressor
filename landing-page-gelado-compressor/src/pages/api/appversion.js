b7bc19fc042cf78450ed081a2c25fc6b12cf9661
Abraão Morais <moraisabraao@gmail.com>
2023-02-12 02:54:21 -0300

    Update appversion.js

--- landing-page-gelado-compressor/src/pages/api/appversion.js
+++ landing-page-gelado-compressor/src/pages/api/appversion.js
@@ -3,5 +3,5 @@
 export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3369" || "http://localhost:8888");
 
-  res.status(200).json({ name: 'Gelado Compressor', description: 'The best image compressor from web', version: '1.2.5'})
+  res.status(200).json({ name: 'Gelado Compressor', description: 'The best image compressor from web', version: '1.2.4'})
 }
