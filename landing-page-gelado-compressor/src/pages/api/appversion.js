5267ed7b0db00988b6ce56ad736c2f60f1358797
Abraão Morais <moraisabraao@gmail.com>
2023-02-11 23:17:34 -0300

    cors aceptable

--- landing-page-gelado-compressor/src/pages/api/appversion.js
+++ landing-page-gelado-compressor/src/pages/api/appversion.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.status(200).json({ name: 'Gelado Compressor', description: 'The best image compressor from web', version: '1.2.4'})
 }
